a2ce1aef9a20a555f8f2f358219ac235e84ca701
Hamid Abdullah <hamidabdullahofficial@gmail.com>
2024-11-04 00:27:14 -0800

    make request to get data.

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-console.log("Start ............");
-
-setTimeout(() => {
-    console.log('2 sec Wait .......');
-}, 2000);
-
-console.log("Stopping ..........")
+import request from 'request';
+const url = 'https://api.weatherstack.com/current?access_key=327b057d334d347f589105dc7b282d0a&query=New%20York';
+request({url: url}, (errorMessage, response) => {
+    const data = JSON.parse(response.body);
+    console.log({response: data.current});
+})
