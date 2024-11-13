bfea1755542e3bd03da750145a524a6cffa744ef
Hamid Abdullah <hamidabdullahofficial@gmail.com>
2024-11-13 12:05:15 -0800

    learn callback chaning system.

--- app.js
+++ app.js
@@ -2,32 +2,33 @@ import request from "request";
 import { getCoordinates } from "./src/utils/geoCoding.js";
 import { weatherStackApi } from "./src/utils/weatherStackApi.js";
 
-// const address = "Pakistan";
-// getCoordinates(address, (error, userLocationDetails) => {
-//     if (error) {
-//         console.log(error);
-//     } else {
-//         console.log("Coordinates:", userLocationDetails);
-//         console.log(
-//             `Your Current Location => ${userLocationDetails.placeName},
-//              Latitude: ${userLocationDetails?.latitude},
-//              Longitude: ${userLocationDetails?.longitude}`
-//         );
-//     }
-// });
+const address = "Pakistan";
 
-const location = { latitude: 37.8367, longitude: -122.4233 };
-weatherStackApi(location, (error, data) => {
+getCoordinates(address, (error, userLocationDetails) => {
     if (error) {
-        console.log({ Weather_Stack_Api_Error: error });
-        return;
+        console.log(error);
+    } else {
+        console.log("Coordinates:", userLocationDetails);
+        console.log(
+            `Your Current Location => ${userLocationDetails.placeName},
+            Latitude: ${userLocationDetails?.latitude},
+            Longitude: ${userLocationDetails?.longitude}`
+        );
     }
-
-    console.log({
-        Weather_Stack_Api_Response: {
-            description: data.description,
-            temperature: `${data.temperature}°F`,
-            feelslike: `${data.feelslike}°F`
+    const location = { latitude: userLocationDetails.latitude, longitude: userLocationDetails.longitude };
+    weatherStackApi(location, (error, data) => {
+        if (error) {
+            console.log({ Weather_Stack_Api_Error: error });
+            return;
         }
+
+        console.log({
+            Weather_Stack_Api_Response: {
+                description: data.description,
+                temperature: `${data.temperature}°F`,
+                feelslike: `${data.feelslike}°F`
+            }
+        });
     });
 });
+
